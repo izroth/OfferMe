d7dadfca9a5f273ecf7f6d6cea502dcdb5faa911
izroth <lakhansharma889027@gmail.com>
2023-08-03 14:54:02 +0530

    added json2xls

--- controllers/convertdataintoexcel.js
+++ controllers/convertdataintoexcel.js
@@ -3,3 +3,19 @@ const users = require('../models/user.js');
 const errors = {
     product_name: 'Product name is required.',
 };
+//json2xls
+const json2xls = require('json2xls');
+const fs = require('fs');
+const axios = require('axios');
+const convertdataintoexcel = (req, res) => {
+    try {
+        const userid = req.userId;
+        console.log(userid);
+        
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'An error occurred.' });
+    }
+
+}
