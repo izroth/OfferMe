8edbded96ee68521b5d14835a0dff8b17a24c6c4
izroth <lakhansharma889027@gmail.com>
2023-09-01 20:29:07 +0530

    done

--- controllers/truecaller.js
+++ controllers/truecaller.js
@@ -1,28 +1,35 @@
-const truecallerjs = require('truecallerjs')
+const truecallerjs = require('truecallerjs');
 
-const errors = {
 
-}
+const errors = {};
 
 const truecaller = async (req, res) => {
-
-    try{
-        const phonenumber = req.body.phonenumber;
-        if(!phonenumber){
+    try {
+        const { phone, countrycode } = req.body;
+        if (!phone) {
             throw new Error('nophone');
         }
-        const findinfo = await truecallerjs.search(phonenumber);
-        if(!findinfo){
-            throw new Error('noinfo');
+        //check the length of phone number
+        if (phone.length < 10) {
+            throw new Error('invalidphone');
         }
-        res.status(200).json({message:'success',data:findinfo});
-        
 
-    }
-    catch(err){
-        console.log(err);
-        res.status(500).json({ message:err[errors]|| 'An error occurred.' });
-    }
+        const searchData = {
+            number: phone,
+            countryCode: "IN",
+            installationId: "a1i0C--heJuzDFS-ENZJpaXXepHFuYu71Sbqh_NgCk3ZY9Rxyd40XYSaU6wmq7e6",
+          };
 
-}
+        // Create a SearchData object
+        const response = await truecallerjs.search(searchData);
+
+
+
+        res.status(200).json({ message: 'success', data: response.json() });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: err.message || 'An error occurred.' });
+    }
+};
+
 module.exports = truecaller;
--- routes/search.js
+++ routes/search.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 
 const findproducts = require('../controllers/findproducts.js');
+const truecaller = require('../controllers/truecaller.js');
 
 router.get('/findproducts', findproducts);
+router.post('/truecaller', truecaller);
 
 module.exports = router;
