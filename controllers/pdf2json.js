9b67deddadbf01fa8342a6a7ecb7a8679285b461
izroth <lakhansharma889027@gmail.com>
2023-09-03 16:45:13 +0530

    done

--- controllers/pdf2json.js
+++ controllers/pdf2json.js
@@ -1,5 +1,5 @@
-const pdf = require('pdf-parse');
 const multer = require('multer');
+const pdf = require('pdf-parse');
 
 // Configure Multer for file uploads
 const storage = multer.memoryStorage();
@@ -10,39 +10,31 @@ const pdf2json = async (req, res) => {
         // Use Multer to handle file uploads
         upload.single('pdf')(req, res, async (err) => {
             if (err instanceof multer.MulterError) {
-           
                 return res.status(400).json({ message: 'File upload error' });
             } else if (err) {
-                
                 console.error(err);
                 return res.status(500).json({ message: 'An error occurred' });
             }
 
             try {
-               
                 if (!req.file) {
                     return res.status(400).json({ message: 'No file uploaded' });
                 }
 
-                // Access the uploaded file
+                // Access the uploaded file buffer
                 const fileBuffer = req.file.buffer;
 
-                // Convert the file buffer to a string
-                const data = fileBuffer.toString();
-
-                // Parse the PDF data
-                const pdfData = await pdf(data);
-
-                // Get the text content from the PDF
+                // Convert the PDF to text using pdf-parse
+                const pdfData = await pdf(fileBuffer);
                 const text = pdfData.text;
 
-                // Split the text by new lines
+                // Split the text by new line
                 const textArray = text.split('\n');
 
                 // Remove empty lines
                 const filteredText = textArray.filter((line) => line.trim() !== '');
 
-                // Send the filtered text as a JSON response
+                // Send the filtered text in the response
                 res.status(200).json({ message: 'success', data: filteredText });
             } catch (err) {
                 console.error(err);
