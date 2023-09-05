5ec15baa1976126b88a42c9b3bf23350e443b40e
izroth <lakhansharma889027@gmail.com>
2023-09-05 13:55:59 +0530

    done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//create a new express app
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -7,7 +7,7 @@ const redis = require('redis');
 require('dotenv').config();
 const authentication =require('./middlewares/useridmiddleware.js')
 const redismiddleware = require('./middlewares/redis.js');
-//create a middleware for cors
+
 const corsOptions = {
     origin: 'http://localhost:4200',
     optionsSuccessStatus: 200
@@ -24,13 +24,11 @@ const products = require('./routes/products.js');
 const users = require('./routes/user.js');
 const search = require('./routes/search.js');
 
-//use the routes
+
 
 app.use('/products',[authentication,redismiddleware], products);
 app.use('/users', users);
 app.use('/search', search);
 
-
-//if server is running, print this message
 app.listen(port, () => console.log(`Offer me listening on port ${port}!`));
 app.get('/', (req, res) => res.send('Hello World!'));
