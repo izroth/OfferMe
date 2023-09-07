e2328384e8d735e4a61764f786c1126cf408f711
izroth <lakhansharma889027@gmail.com>
2023-09-07 17:49:31 +0530

    sdss

--- controllers/findproducts.js
+++ controllers/findproducts.js
@@ -1,3 +1,4 @@
+const { search } = require('truecallerjs');
 const productinfo = require('../models/productinfo');
 const errors = {
     nofilter: 'No filter is provided.',
@@ -25,13 +26,19 @@ const findproducts = async (req, res) => {
                 }
             }
         });
+        // const product  = await productinfo.find({"":$regex:{ ".*"+search+".*"}});
+        // if(!product){
+        //     res.status(200).json({ message: 'No matching results found.' });
+        // }
 
         if (matchingResults.length === 0) {
             res.status(404).json({ message: 'No matching results found.' });
         }
+    
 
 
-        res.status(200).json({ results: matchingResults });
+        res.status(200).json({ results: product });
+        
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: error[errors] || 'An error occurred.' });
